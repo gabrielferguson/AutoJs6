da96f0c21faff4ba0abea956ad56ea74b19c098b
Gabriel Ferguson <gabriel.ferguson@sjsu.edu>
2025-10-02 17:08:32 +0800

    Create DatabaseCipherTransactionWrapper.java

---
+++ app/src/main/java/org/autojs/autojs/core/database/DatabaseCipherTransactionWrapper.java
@@ -0,0 +1,8 @@
+package org.autojs.autojs.core.database;
+
+public class DatabaseCipherTransactionWrapper extends org.autojs.autojs.core.database.Transaction {
+    
+    public DatabaseCipherTransactionWrapper(DatabaseCipher database) {
+        super(database);
+    }
+}
