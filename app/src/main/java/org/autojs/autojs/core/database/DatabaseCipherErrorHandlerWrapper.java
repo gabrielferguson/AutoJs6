f711fb34b0a5126137f016989086f8dcebfa3c4c
Gabriel Ferguson <gabriel.ferguson@sjsu.edu>
2025-10-02 19:48:29 +0800

    Update DatabaseCipherErrorHandlerWrapper.java

--- app/src/main/java/org/autojs/autojs/core/database/DatabaseCipherErrorHandlerWrapper.java
+++ app/src/main/java/org/autojs/autojs/core/database/DatabaseCipherErrorHandlerWrapper.java
@@ -1,5 +1,6 @@
 package org.autojs.autojs.core.database;
 
+import android.database.sqlite.SQLiteException;  // 注意：来自 android.database.sqlite 包
 import net.zetetic.database.DatabaseErrorHandler;
 import net.zetetic.database.sqlcipher.SQLiteDatabase;
 
@@ -12,10 +13,10 @@ public final class DatabaseCipherErrorHandlerWrapper implements DatabaseErrorHan
     }
 
     @Override
-    public void onCorruption(SQLiteDatabase sqLiteDatabase) {
-        // 只有一个参数，不需要 SQLiteException
+    public void onCorruption(SQLiteDatabase dbObj, SQLiteException exception) {
+        // 正确的签名：两个参数，SQLiteException 来自 android.database.sqlite
         if (mCallback != null) {
-            mCallback.onCorruption(sqLiteDatabase);
+            mCallback.onCorruption(dbObj);
         }
     }
 }
